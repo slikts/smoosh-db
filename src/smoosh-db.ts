c4ecfbe58a0e4c39c14f43a94baef64b15c66495
slikts <dabas@untu.ms>
2018-08-09 22:06:30 +0300

    asynclog

--- src/smoosh-db.ts
+++ src/smoosh-db.ts
@@ -1,6 +1,7 @@
 import { deepProxy } from '@slikts/deepproxy'
 import { memoize } from 'tuplerone'
 import { promises } from 'fs'
+import AsyncQueue from '@slikts/asyncqueue'
 
 export const thenOnce = memoize(Promise.prototype.then)
 const encoding = 'utf8'
@@ -19,19 +20,32 @@ const write = async (
   await fileHandle.writeFile(JSON.stringify(value, replacer, 2), { encoding })
 }
 
+export enum Event {
+  WriteStart,
+  WriteEnd,
+  Update,
+}
+
+/**
+ * Recursively wraps object with `Proxy` to auto-sync changes to a JSON file.
+ */
 export const liveJSON = async <A extends object>(
   fileName: string,
   init: () => A,
   fs = promises,
   replacer: any = null,
 ): Promise<A> => {
+  const events: AsyncQueue<Event> = new AsyncQueue()
   const fileHandle = await fs.open(fileName, 'a+')
   const root = await read(fileHandle, init)
   let status: Promise<void> | null = null
   const update = async (): Promise<void> => {
     // Set status to writing so that changes would trigger a new update
+    // TODO factor out the delayed writing file
     status = write(fileHandle, root, replacer)
+    events.push(Event.WriteStart)
     await status
+    events.push(Event.WriteEnd)
     // Clear status after writing
     status = null
   }
